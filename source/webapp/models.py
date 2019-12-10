435d99715169019c7341e3d3e747fa9f7a1ffd10
DinaraKA <d.karybekova@gmail.com>
2019-12-11 03:23:29 +0600

    Added model Quote, registered in admin and migrated.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+NEW_QUOTE = 'new'
+CONFIRMED_QUOTE = 'confirmed'
+QUOTE_STATUS_CHOICES = (
+    (NEW_QUOTE, 'New'),
+    (CONFIRMED_QUOTE, 'Confirmed')
+)
+
+
+class Quote(models.Model):
+    text = models.TextField(max_length=1000, verbose_name="Text")
+    author = models.CharField(max_length=20, verbose_name='Added by')
+    email = models.EmailField(verbose_name='Email')
+    status = models.CharField(max_length=20, choices=QUOTE_STATUS_CHOICES, default=NEW_QUOTE, verbose_name='Status')
+    rating = models.IntegerField(default=0, verbose_name='Raiting')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Added date")
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Quote'
+        verbose_name_plural = 'Quotes'
+
+
